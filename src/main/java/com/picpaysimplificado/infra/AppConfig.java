97108379bb6d40912c77b15e85b28c9370dc012a
mairanathalya <maira.nathalya@gmail.com>
2024-04-16 13:28:50 -0300

    feat: create App configuration

--- src/main/java/com/picpaysimplificado/infra/AppConfig.java
+++ src/main/java/com/picpaysimplificado/infra/AppConfig.java
@@ -1,2 +1,14 @@
-package com.picpaysimplificado.infra;public class AppConfig {
+package com.picpaysimplificado.infra;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+    //indicating to spring which class it has inject to other classes that are using this class as dependency
+    @Bean
+    public RestTemplate restTemplate () {
+        return new RestTemplate();
+    }
 }
