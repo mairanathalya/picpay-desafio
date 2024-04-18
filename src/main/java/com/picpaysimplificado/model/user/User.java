e7d9ba5f18f07bb0c8a595082d599e1c4e84c388
mairanathalya <maira.nathalya@gmail.com>
2024-04-18 15:09:01 -0300

    feat: updating User, UserDTO and UserService

--- src/main/java/com/picpaysimplificado/model/user/User.java
+++ src/main/java/com/picpaysimplificado/model/user/User.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.model.user;
 
+import com.picpaysimplificado.dto.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -33,4 +34,14 @@ public class User {
 
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.document = data.document();
+        this.email = data.email();
+        this.password = data.password();
+        this.balance = data.balance();
+        this.userType = data.userType();
+    }
 }
--- src/main/java/com/picpaysimplificado/service/UserService.java
+++ src/main/java/com/picpaysimplificado/service/UserService.java
@@ -1,5 +1,6 @@
 package com.picpaysimplificado.service;
 
+import com.picpaysimplificado.dto.UserDTO;
 import com.picpaysimplificado.model.user.User;
 import com.picpaysimplificado.model.user.UserType;
 import com.picpaysimplificado.repository.UserRepository;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -27,6 +29,16 @@ public class UserService {
     //method to allow the transaction service to manipulate users without needing the repository user
     public User findUserById(Long id) throws Exception{
         return this.repository.findUserById(id).orElseThrow( () ->new Exception("Usuário não encontrado"));
+    }
+
+    public User createUser(UserDTO data){
+        User newUser = new User(data);
+        this.saveUser(newUser);
+        return newUser;
+    }
+
+    public List<User> getAllUsers(){
+       return this.repository.findAll();
     }
 
     public void saveUser (User user){
